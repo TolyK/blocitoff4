9ed83e193c3ffc305a3a62c4dd58c209e9cb7000
Toly <anatoly.kvitnitsky@gmail.com>
2015-02-19 23:02:23 +0000

    create user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    redirect_to @user if @user.save
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
